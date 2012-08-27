0d1ebb24c0ea72eaf14763c94802aed8c0554a7e
Jason R. Coombs <jaraco@jaraco.com>
2012-08-27 05:30:12 -0400

    Reindent long lines

--- popquotes/pmxbot.py
+++ popquotes/pmxbot.py
@@ -24,9 +24,12 @@ def bartletts(lib, nick, qsearch):
 # declare all of the popquotes commands
 quote_libs = (
 	# name, aliases, doc, lib
-	('bender', ('bend',), 'Quote Bender, a la http://en.wikiquote.org/wiki/Futurama', 'bender'),
-	('zoidberg', ('zoid',), 'Quote Zoidberg, a la http://en.wikiquote.org/wiki/Futurama', 'zoid'),
-	('simpsons', ('simp',), 'Quote the Simpsons, a la http://snpp.com/', 'simpsons'),
+	('bender', ('bend',),
+		'Quote Bender, a la http://en.wikiquote.org/wiki/Futurama', 'bender'),
+	('zoidberg', ('zoid',),
+		'Quote Zoidberg, a la http://en.wikiquote.org/wiki/Futurama', 'zoid'),
+	('simpsons', ('simp',), 'Quote the Simpsons, a la http://snpp.com/',
+		'simpsons'),
 	('hal', ('2001',), 'HAL 9000', 'hal'),
 	('grail', (), 'I questing baby', 'grail'),
 	('anchorman', (), 'Quote Anchorman.', 'anchorman'),
@@ -37,7 +40,8 @@ quote_libs = (
 # create the popquotes commands per the declarations above
 def make_command(name, aliases, doc, lib):
 	# create a command function with the pmxbot command signature
-	cmd_func = lambda client, event, channel, nick, rest: bartletts(lib, nick, rest)
+	cmd_func = lambda client, event, channel, nick, rest: bartletts(lib, nick,
+		rest)
 	# register the new func as a command
 	pmxbot_cmd = command(name, aliases=aliases, doc=doc)(cmd_func)
 	# add the command to the global namespace so it can be tested
